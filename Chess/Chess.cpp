49d3fee24b17042643bd84b07b1bcde0cc670201
kiwiivanya128@gmail.com <kiwiivanya128@gmail.com>
2023-02-22 22:45:01 +0200

    realesing a version

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -7,71 +7,14 @@
 
 int main(int argc, char const* argv[])
 {
-	auto nt = NetworkTeller::GetInstance();
 	auto mc = chess_lib::MoveController::GetInstance();
 	auto r = ConRenderer();
 	auto board = chess_lib::Board();
-	auto issr = false;
-	auto side = false;
-	auto ip = std::string();
 
-	std::cout << "Init game(0 - client/1 - server):";
-	std::cin >> issr;
-
-	if (issr)
+	while ((!mc.IsMate(board) && !mc.IsStalemate(board) && !mc.IsDraw(board)))
 	{
-		nt.StartAsServer();
-		while (!nt.IsSocketGot());
-		side = nt.InitGameFromServer();
-		while (nt.IsConnection() && (!mc.IsMate(board) && !mc.IsStalemate(board) && !mc.IsDraw(board)))
-		{
-			if (nt.IsIncome())
-			{
-				if (nt.IsSpecial())
-					nt.ReadSpecialMessage();
-				if (!nt.IsConnection())
-					break;
-				RecreateMove(board, nt.RecreateMove());
-			}
-			if (board.GetIsWhiteMove() != side)
-			{
-				std::cout << "Waiting...";
-				std::cout << "\r\r\r\r\r\r\r\r\r\r";
-				continue;
-			}
-			std::cout << r.Render(board, side);
-			auto recr = FormQuire(board);
-			nt.TellMove(recr.move, recr.prom);
-		}
-		nt.Disconnect();
-	}
-	else
-	{
-		std::cout << "IP address:";
-		std::cin >> ip;
-		nt.StartAsClient(ip);
-		while (!nt.IsIncome());
-		side = nt.ReadSpecialMessage();
-		while (nt.IsConnection() && (!mc.IsMate(board) && !mc.IsStalemate(board) && !mc.IsDraw(board)))
-		{
-			if (nt.IsIncome())
-			{
-				if (nt.IsSpecial())
-					nt.ReadSpecialMessage();
-				if (!nt.IsConnection())
-					break;
-				RecreateMove(board, nt.RecreateMove());
-			}
-			if (board.GetIsWhiteMove() != side)
-			{
-				std::cout << "Waiting...";
-				std::cout << "\r\r\r\r\r\r\r\r\r\r";
-				continue;
-			}
-			auto recr = FormQuire(board);
-			nt.TellMove(recr.move, recr.prom);
-		}
-		nt.Disconnect();
+		std::cout << r.Render(board, board.GetIsWhiteMove());
+		TypeInterface::FormQuire(board);
 	}
 	
 	if (mc.IsMate(board))
--- Chess/TypeInterface.h
+++ Chess/TypeInterface.h
@@ -2,64 +2,66 @@
 #include <iostream>
 #include "NetworkTeller.h"
 
-
-inline NetworkTeller::net_move FormQuire(chess_lib::Board& board)
+class TypeInterface
 {
-	auto m1 = std::string();
-	auto m2 = std::string();
-	auto mc = chess_lib::MoveController::GetInstance();
-	auto mv = std::shared_ptr<NetworkTeller::net_move>();
-	std::cout << "Check: " << mc.IsCheck(board) << '\n';
-ty_again_move:
-	std::cout << "Move a piece in: ";
-	std::cin >> m1;
-	std::cout << "Move a piece to: ";
-	std::cin >> m2;
-	if (!mc.ConfirmMove(board, chess_lib::Move{ board.ConvertToIndex(m1),board.ConvertToIndex(m2) }))
+public:
+	inline static NetworkTeller::net_move FormQuire(chess_lib::Board& board)
 	{
-		std::cout << "no move\n";
-		goto ty_again_move;
+		auto m1 = std::string();
+		auto m2 = std::string();
+		auto mc = chess_lib::MoveController::GetInstance();
+		auto mv = std::shared_ptr<NetworkTeller::net_move>();
+		std::cout << "Check: " << mc.IsCheck(board) << '\n';
+	ty_again_move:
+		std::cout << "Move a piece in: ";
+		std::cin >> m1;
+		std::cout << "Move a piece to: ";
+		std::cin >> m2;
+		if (!mc.ConfirmMove(board, chess_lib::Move{ board.ConvertToIndex(m1),board.ConvertToIndex(m2) }))
+		{
+			std::cout << "no move\n";
+			goto ty_again_move;
+		}
+		mv = std::make_shared<NetworkTeller::net_move>(NetworkTeller::net_move{ chess_lib::Move{ board.ConvertToIndex(m1),board.ConvertToIndex(m2) }, chess_lib::Board::PromotionChoice::none });
+		if (board.NeedPromotion())
+		{
+			std::string s;
+		ty_again_promote:
+			std::cout << "Choose a promotion piece in " << board.ConvertFromIndex(board.GetPreviousMove()->GetP2()) << ": ";
+			std::cin >> s;
+			if (s == "queen")
+			{
+				mc.PromoteTo(board, chess_lib::Board::PromotionChoice::queen);
+				mv->prom = chess_lib::Board::PromotionChoice::queen;
+			}
+			else if (s == "bishop")
+			{
+				mc.PromoteTo(board, chess_lib::Board::PromotionChoice::bishop);
+				mv->prom = chess_lib::Board::PromotionChoice::bishop;
+			}
+			else if (s == "knight")
+			{
+				mc.PromoteTo(board, chess_lib::Board::PromotionChoice::knight);
+				mv->prom = chess_lib::Board::PromotionChoice::knight;
+			}
+			else if (s == "rook")
+			{
+				mc.PromoteTo(board, chess_lib::Board::PromotionChoice::rook);
+				mv->prom = chess_lib::Board::PromotionChoice::rook;
+			}
+			else
+				goto ty_again_promote;
+		}
+		return *mv;
 	}
-	mv = std::make_shared<NetworkTeller::net_move>(NetworkTeller::net_move{ chess_lib::Move{ board.ConvertToIndex(m1),board.ConvertToIndex(m2) }, chess_lib::Board::PromotionChoice::none });
-	if (board.NeedPromotion())
+
+	inline static bool RecreateMove(chess_lib::Board& board, NetworkTeller::net_move mv)
 	{
-		std::string s;
-	ty_again_promote:
-		std::cout << "Choose a promotion piece in " << board.ConvertFromIndex(board.GetPreviousMove()->GetP2()) << ": ";
-		std::cin >> s;
-		if (s == "queen")
-		{
-			mc.PromoteTo(board, chess_lib::Board::PromotionChoice::queen);
-			mv->prom = chess_lib::Board::PromotionChoice::queen;
-		}
-		else if (s == "bishop")
-		{
-			mc.PromoteTo(board, chess_lib::Board::PromotionChoice::bishop);
-			mv->prom = chess_lib::Board::PromotionChoice::bishop;
-		}
-		else if (s == "knight")
-		{
-			mc.PromoteTo(board, chess_lib::Board::PromotionChoice::knight);
-			mv->prom = chess_lib::Board::PromotionChoice::knight;
-		}
-		else if (s == "rook")
-		{
-			mc.PromoteTo(board, chess_lib::Board::PromotionChoice::rook);
-			mv->prom = chess_lib::Board::PromotionChoice::rook;
-		}
-		else
-			goto ty_again_promote;
+		auto conf = false;
+		auto mc = chess_lib::MoveController::GetInstance();
+		conf = mc.ConfirmMove(board, mv.move);
+		if (board.NeedPromotion())
+			mc.PromoteTo(board, mv.prom);
+		return conf;
 	}
-	return *mv;
-}
-
-inline bool RecreateMove(chess_lib::Board& board, NetworkTeller::net_move mv)
-{
-	auto conf = false;
-	auto mc = chess_lib::MoveController::GetInstance();
-	conf = mc.ConfirmMove(board, mv.move);
-	if (board.NeedPromotion())
-		mc.PromoteTo(board, mv.prom);
-	return conf;
-}
-
+};
